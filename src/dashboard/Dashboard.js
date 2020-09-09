ed62765b0ad976d54de2c6bc4a4ae26d0d8fb16c
Federico Correa <you@example.com>
2020-09-09 13:46:52 -0300

    Se agrega el maquetado del dashboard

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -3,9 +3,308 @@ import "./dashboard.css";
 
 const Dashboard = () => {
     return (
-        <div className="containerdashboard">
-            <h1 className="text-white text-center centrado404">404 not found.</h1>
+        <> 
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+        <a className="navbar-brand" href="index.html">Rolling Firebase</a>
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav navbar-sidenav" id="exampleAccordion">
+            <li className="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+              <a className="nav-link" href="index.html">
+                <i className="fa fa-fw fa-dashboard"></i>
+                <span className="nav-link-text">Dashboard</span>
+              </a>
+            </li>
+            <li className="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
+              <a className="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents" data-parent="#exampleAccordion">
+                <i className="fa fa-fw fa-wrench"></i>
+                <span className="nav-link-text">Components</span>
+              </a>
+              <ul className="sidenav-second-level collapse" id="collapseComponents">
+                <li>
+                  <a href="navbar.html">Navbar</a>
+                </li>
+                <li>
+                  <a href="cards.html">Cards</a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item" data-toggle="tooltip" data-placement="right" title="Example Pages">
+              <a className="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseExamplePages" data-parent="#exampleAccordion">
+                <i className="fa fa-fw fa-file"></i>
+                <span className="nav-link-text">Example Pages</span>
+              </a>
+              <ul className="sidenav-second-level collapse" id="collapseExamplePages">
+                <li>
+                  <a href="login.html">Login Page</a>
+                </li>
+                <li>
+                  <a href="register.html">Registration Page</a>
+                </li>
+                <li>
+                  <a href="forgot-password.html">Forgot Password Page</a>
+                </li>
+                <li>
+                  <a href="blank.html">Blank Page</a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item" data-toggle="tooltip" data-placement="right" title="Menu Levels">
+              <a className="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMulti" data-parent="#exampleAccordion">
+                <i className="fa fa-fw fa-sitemap"></i>
+                <span className="nav-link-text">Menu Levels</span>
+              </a>
+              <ul className="sidenav-second-level collapse" id="collapseMulti">
+                <li>
+                  <a href="#">Second Level Item</a>
+                </li>
+                <li>
+                  <a href="#">Second Level Item</a>
+                </li>
+                <li>
+                  <a href="#">Second Level Item</a>
+                </li>
+                <li>
+                  <a className="nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMulti2">Third Level</a>
+                  <ul className="sidenav-third-level collapse" id="collapseMulti2">
+                    <li>
+                      <a href="#">Third Level Item</a>
+                    </li>
+                    <li>
+                      <a href="#">Third Level Item</a>
+                    </li>
+                    <li>
+                      <a href="#">Third Level Item</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item" data-toggle="tooltip" data-placement="right" title="Link">
+              <a className="nav-link" href="#">
+                <i className="fa fa-fw fa-link"></i>
+                <span className="nav-link-text">Link</span>
+              </a>
+            </li>
+          </ul>
+          <ul className="navbar-nav sidenav-toggler">
+            <li className="nav-item">
+              <a className="nav-link text-center" id="sidenavToggler">
+                <i className="fa fa-fw fa-angle-left"></i>
+              </a>
+            </li>
+          </ul>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" data-toggle="modal" data-target="#exampleModal">
+                <i className="fa fa-fw fa-sign-out"></i>Logout</a>
+            </li>
+          </ul>
         </div>
+      </nav>
+      <div className="content-wrapper">
+        <div className="container-fluid">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <a href="#">Dashboard</a>
+            </li>
+            <li className="breadcrumb-item active">My Dashboard</li>
+          </ol>
+
+          <div className="row">
+            <div className="col-xl-3 col-sm-6 mb-3">
+              <div className="card text-white bg-primary o-hidden h-100">
+                <div className="card-body">
+                  <div className="card-body-icon">
+                    <i className="fa fa-fw fa-comments"></i>
+                  </div>
+                  <div className="mr-5">26 New Messages!</div>
+                </div>
+                <a className="card-footer text-white clearfix small z-1" href="#">
+                  <span className="float-left">View Details</span>
+                  <span className="float-right">
+                    <i className="fa fa-angle-right"></i>
+                  </span>
+                </a>
+              </div>
+            </div>
+            <div className="col-xl-3 col-sm-6 mb-3">
+              <div className="card text-white bg-warning o-hidden h-100">
+                <div className="card-body">
+                  <div className="card-body-icon">
+                    <i className="fa fa-fw fa-list"></i>
+                  </div>
+                  <div className="mr-5">11 New Tasks!</div>
+                </div>
+                <a className="card-footer text-white clearfix small z-1" href="#">
+                  <span className="float-left">View Details</span>
+                  <span className="float-right">
+                    <i className="fa fa-angle-right"></i>
+                  </span>
+                </a>
+              </div>
+            </div>
+            <div className="col-xl-3 col-sm-6 mb-3">
+              <div className="card text-white bg-success o-hidden h-100">
+                <div className="card-body">
+                  <div className="card-body-icon">
+                    <i className="fa fa-fw fa-shopping-cart"></i>
+                  </div>
+                  <div className="mr-5">123 New Orders!</div>
+                </div>
+                <a className="card-footer text-white clearfix small z-1" href="#">
+                  <span className="float-left">View Details</span>
+                  <span className="float-right">
+                    <i className="fa fa-angle-right"></i>
+                  </span>
+                </a>
+              </div>
+            </div>
+            <div className="col-xl-3 col-sm-6 mb-3">
+              <div className="card text-white bg-danger o-hidden h-100">
+                <div className="card-body">
+                  <div className="card-body-icon">
+                    <i className="fa fa-fw fa-support"></i>
+                  </div>
+                  <div className="mr-5">13 New Tickets!</div>
+                </div>
+                <a className="card-footer text-white clearfix small z-1" href="#">
+                  <span className="float-left">View Details</span>
+                  <span className="float-right">
+                    <i className="fa fa-angle-right"></i>
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+
+         <form className="form-inline my-2 my-lg-3 mr-lg-2">
+                <div className="input-group">
+                  <input className="form-control" type="text" placeholder="Search for..." />
+                  <span className="input-group-append">
+                    <button className="btn btn-primary" type="button">
+                      <i className="fa fa-search"></i>
+                    </button>
+                  </span>
+                </div>
+              </form>
+          <div className="card mb-3">
+            <div className="card-header">
+              <i className="fa fa-table"></i> Data Table Example</div>
+            <div className="card-body">
+              <div className="table-responsive">
+                <table className="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Position</th>
+                      <th>Office</th>
+                      <th>Age</th>
+                      <th>Start date</th>
+                      <th>Salary</th>
+                    </tr>
+                  </thead>
+                  <tfoot>
+                    <tr>
+                      <th>Name</th>
+                      <th>Position</th>
+                      <th>Office</th>
+                      <th>Age</th>
+                      <th>Start date</th>
+                      <th>Salary</th>
+                    </tr>
+                  </tfoot>
+                  <tbody>
+                    <tr>
+                      <td>Tiger Nixon</td>
+                      <td>System Architect</td>
+                      <td>Edinburgh</td>
+                      <td>61</td>
+                      <td>2011/04/25</td>
+                      <td>$320,800</td>
+                    </tr>
+                    <tr>
+                      <td>Garrett Winters</td>
+                      <td>Accountant</td>
+                      <td>Tokyo</td>
+                      <td>63</td>
+                      <td>2011/07/25</td>
+                      <td>$170,750</td>
+                    </tr>
+                    <tr>
+                      <td>Ashton Cox</td>
+                      <td>Junior Technical Author</td>
+                      <td>San Francisco</td>
+                      <td>66</td>
+                      <td>2009/01/12</td>
+                      <td>$86,000</td>
+                    </tr>
+                    <tr>
+                      <td>Cedric Kelly</td>
+                      <td>Senior Javascript Developer</td>
+                      <td>Edinburgh</td>
+                      <td>22</td>
+                      <td>2012/03/29</td>
+                      <td>$433,060</td>
+                    </tr>
+                    <tr>
+                      <td>Airi Satou</td>
+                      <td>Accountant</td>
+                      <td>Tokyo</td>
+                      <td>33</td>
+                      <td>2008/11/28</td>
+                      <td>$162,700</td>
+                    </tr>
+                    <tr>
+                      <td>Brielle Williamson</td>
+                      <td>Integration Specialist</td>
+                      <td>New York</td>
+                      <td>61</td>
+                      <td>2012/12/02</td>
+                      <td>$372,000</td>
+                    </tr>
+                    <tr>
+                      <td>Herrod Chandler</td>
+                      <td>Sales Assistant</td>
+                      <td>San Francisco</td>
+                      <td>59</td>
+                      <td>2012/08/06</td>
+                      <td>$137,500</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div className="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+          </div>
+        </div>
+        <a className="scroll-to-top rounded" href="#page-top">
+          <i className="fa fa-angle-up"></i>
+        </a>
+
+        <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                <button className="close" type="button" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">×</span>
+                </button>
+              </div>
+              <div className="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+              <div className="modal-footer">
+                <button className="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                <a className="btn btn-primary" href="login.html">Logout</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </>
     );
 };
 
